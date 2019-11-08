23a8af8c8769d4c9c0d69cfc56427590c65706ea
coopergrinnell <coopergrinnell@gmail.com>
2019-11-07 22:03:39 -0500

    1-4 have modals

--- js/page.js
+++ js/page.js
@@ -36,3 +36,14 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 } 
+
+$(".button").on("click", function() {
+  var modal = $(this).data("modal");
+  $(modal).show(); 
+});
+$(".modal").on("click", function(e) { 
+  var className = e.target.className; 
+  if(className === "modal" || className === "close"){ 
+    $(this).closest(".modal").hide(); 
+  } 
+}); 
